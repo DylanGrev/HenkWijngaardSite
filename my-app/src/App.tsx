798fd2dd053636ed26e9a1f4425bc43b2004c9ee
Dylan <Dylangreven2@outlook.com>
2025-12-15 14:28:56 +0100

    layout

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './index.tsx'
+import spotifyIcon from './images/icons/spotify_icon.png';
+import facebookIcon from './images/icons/facebook_icon.png';
+import youtubeIcon from './images/icons/youtube_icon.png';
+import HomepageBanner from './images/Henk_Wijngaard_banner02.png';
+import background from './images/Henk_Wijngaard_background.png';
 
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app" style={{ backgroundImage: `url(${background})` }}>
+      {/* background applied via inline style on .app */}
+      <nav className="navbar">
+        <div className="nav-container">
+          <div id="logoText">Henk Wijngaard</div>
+
+          <div className="navPages">
+            <button type="button" className="nav-btn" onClick={() => window.location.href = '/home'}>Home</button>
+            <button type="button" className="nav-btn" onClick={() => window.location.href = '/over'}>Over</button>
+            <button type="button" className="nav-btn" onClick={() => window.location.href = '/agenda'}>Agenda</button>
+            <button type="button" className="nav-btn" onClick={() => window.location.href = '/contact'}>Contact</button>
+          </div>
+
+          <div className="navSocials">
+            <a href="https://open.spotify.com/artist/0EyzZUO2ycn0hLjKDFT7Hb?si=iPFRXhvOSm6ervMQwLOUaw" target="_blank" rel="noopener noreferrer">
+              <img src={spotifyIcon} alt="Spotify" className="socialIcon" />
+            </a>
+
+            <a href="https://www.facebook.com/henkwijngaardofficial" target="_blank" rel="noopener noreferrer">
+              <img src={facebookIcon} alt="Facebook" className="socialIcon" />
+            </a>
+
+            <a href="https://www.youtube.com/@Henkiewijngaard" target="_blank" rel="noopener noreferrer">
+              <img src={youtubeIcon} alt="YouTube" className="socialIcon" />
+            </a>
+          </div>
+
+        </div>
+      </nav>
+      <header className="banner" id="home">
+        <img src={HomepageBanner} alt="" />
+      </header>
+      <footer className="footer">
+
+        <p className='footerText'>© 2025 Henk Wijngaard / Picobello BV. All rights reserved.</p>  
+
+      </footer>
+    </div>
+
+    
+    
   )
 }
 
