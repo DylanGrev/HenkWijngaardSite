906579084df8dbfb5899ed2378fbb7cf77f0e2fa
Dylan <Dylangreven2@outlook.com>
2025-12-16 11:34:43 +0100

    index met app gekoppeld fix

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,60 +1,41 @@
 import './App.css'
 
 import './index.tsx'
-import spotifyIcon from './images/icons/spotify_icon.png';
-import facebookIcon from './images/icons/facebook_icon.png';
-import youtubeIcon from './images/icons/youtube_icon.png';
-import HomepageBanner from './images/Henk_Wijngaard_banner02.png';
-import background from './images/Henk_Wijngaard_background.png';
+
+import facebookImage from './images/socials/Henk_Wijngaard_Facebook.png';
+import youtubeImage from './images/socials/Henk_Wijngaard_YouTube.png';
+import spotifyImage from './images/socials/Henk_Wijngaard_Spotify.png';
 
 
 function App() {
   return (
-    <div className="app" style={{backgroundImage: `url(${background})`}}>
-      {/* background applied via inline style on .app */}
-      <nav className="navbar">
-        <div className="nav-container">
-          <div id="logoText">Henk Wijngaard</div>
+   
 
-          <div className="navPages">
-            <button type="button" className="nav-btn" onClick={() => window.location.href = '/home'}>Home</button>
-            <button type="button" className="nav-btn" onClick={() => window.location.href = '/over'}>Over</button>
-            <button type="button" className="nav-btn" onClick={() => window.location.href = '/agenda'}>Agenda</button>
-            <button type="button" className="nav-btn" onClick={() => window.location.href = '/contact'}>Contact</button>
+    <div className="app">
+        <div className="contentContainer">
+          <div className="mainColumn">
+            <p className='textBox'>
+              Fijn u te ontmoeten op mijn website. Op deze website vindt u allerlei informatie omtrent mijn optredens, biografie,
+              discografie en kunt u de laatste nieuwsberichten lezen.
+              <br /><br />
+              In de agenda vindt u de data en locaties van mijn optredens en wie weet mag ik u op één van mijn optredens ook verwelkomen.
+              <br /><br />
+              Een muzikale groet van,
+              <br /><br />
+              Henk Wijngaard
+              </p>
           </div>
 
-          <div className="navSocials">
-            <a href="https://open.spotify.com/artist/0EyzZUO2ycn0hLjKDFT7Hb?si=iPFRXhvOSm6ervMQwLOUaw" target="_blank" rel="noopener noreferrer">
-              <img src={spotifyIcon} alt="Spotify" className="socialIcon" />
-            </a>
-
-            <a href="https://www.facebook.com/henkwijngaardofficial" target="_blank" rel="noopener noreferrer">
-              <img src={facebookIcon} alt="Facebook" className="socialIcon" />
-            </a>
-
-            <a href="https://www.youtube.com/@Henkiewijngaard" target="_blank" rel="noopener noreferrer">
-              <img src={youtubeIcon} alt="YouTube" className="socialIcon" />
-            </a>
-          </div>
-
-        </div>
-      </nav>
-      <header className="banner" id="home">
-        <img src={HomepageBanner} alt="" />
-      </header>
-
-
-        <div className="content">
-          <h1>Welkom op de officiële website van Henk Wijngaard!</h1>
-          <p>Hier vind je het laatste nieuws, aankomende optredens en meer over Henk's muzikale reis.</p>
+          <aside className="sideBoxes">
+            <div className="smallStack">
+              <div className="youtubeBox"><a href="https://www.youtube.com/@Henkiewijngaard" target="_blank" rel="noopener noreferrer"><img src={youtubeImage} alt="YouTube" className="socialIcon" /></a></div>
+              <div className='facebookBox'><a href="https://www.facebook.com/henkwijngaardofficial" target="_blank" rel="noopener noreferrer"><img src={facebookImage} alt="Facebook" className="socialIcon" /></a></div>
+            </div>
+            <div className='spotifyBox'><a href="https://open.spotify.com/artist/0EyzZUO2ycn0hLjKDFT7Hb?si=iPFRXhvOSm6ervMQwLOUaw" target="_blank" rel="noopener noreferrer"><img src={spotifyImage} alt="Spotify" className="socialIcon" /></a></div>
+          </aside>
         </div>
 
 
-      <footer className="footer">
-
-        <p className='footerText'>© 2025 Henk Wijngaard / Picobello BV. All rights reserved.</p>  
-
-      </footer>
     </div>
 
     
